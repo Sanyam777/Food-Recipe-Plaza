066dd0fc5cb3040b45f0845ecb24cae508bf4314
Sanyam <sanyammrd56@gmail.com>
2022-09-08 19:26:28 +0530

    Next commit

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -3,7 +3,11 @@ import './RecipeTile.css'
 
 export default function RecipeTile({ recipe }) {
     return (
-        <div className='recipeTile'>
+        <div className='recipeTile'
+            onClick={() => {
+                window.open(recipe["recipe"]["url"])
+            }}
+        >
             <img className='recipeTile__img' src={recipe["recipe"]["image"]} />
             <p className='recipeTile__name'>{recipe["recipe"]["label"]}</p>
         </div>
